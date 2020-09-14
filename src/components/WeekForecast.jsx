ec9930ebe788bfb6c4daa308ba64eebb1f1112de
ppbevilacqua <paolopio.bevilacqua2@gmail.com>
2020-09-14 19:27:14 +0200

    Revert "Inserimento componenti base Redux"
    This reverts commit a006bc2982fd4d0654b611eb7b97f74420981519.

--- src/components/WeekForecast.jsx
+++ src/components/WeekForecast.jsx
@@ -1,11 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import { CardContent } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
-import { fetchForecasts } from "../redux/forecast/forecastAction";
-import { connect } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,13 +19,7 @@ const useStyles = makeStyles((theme) => ({
 const arrayData = ["Title", "Title", "Title", "Title", "Title"];
 let idCount = 1;
 
-const WeekForecast = ({ forecast, fetchForecasts }) => {
-  useEffect(() => {
-    fetchForecasts();
-  }, []);
-
-  console.log("forecast data in WeekForecast", forecast);
-
+const WeekForecast = () => {
   const classes = useStyles();
 
   return (
@@ -45,16 +37,4 @@ const WeekForecast = ({ forecast, fetchForecasts }) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    forecasts: state.forecasts.forecasts,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchForecasts: () => dispatch(fetchForecasts()),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(WeekForecast);
+export default WeekForecast;
