4c1343f1a6ba85d8f21bbafbff12de240c826949
ppbevilacqua <paolopio.bevilacqua2@gmail.com>
2020-09-15 12:27:06 +0200

    Fetch API data
    I dati del meteo ora sono richiesti attraverso le API di Open Weather e destrutturati in base ai dati da visualizzare nel riepilogo settimanale. La key API è stata spostata nel file .env

--- src/redux/forecast/forecastAction.js
+++ src/redux/forecast/forecastAction.js
@@ -4,12 +4,48 @@ import {
   FETCH_FORECAST_FAILURE,
 } from "./forecastType";
 
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 export const fetchForecasts = () => {
   return (dispatch) => {
     dispatch(fetchForecastsRequest());
 
-    return fetch("api.openweathermap.org/data/2.5/forecast?q=Rome&appid=")
+    return fetch(
+      "https://api.openweathermap.org/data/2.5/onecall?lat=41.9109&lon=12.4818&exclude=current,minutely,hourly&units=metric&appid=" +
+        process.env.REACT_APP_WEATHER_API_KEY
+    )
       .then((response) => response.json())
+      .then((forecasts) => {
+        return forecasts.daily.map((daily) => {
+          // conversione data da timestamp in secondi a giorno della settimana
+          const timestamp = daily.dt;
+          const weekDayIndex = new Date(timestamp * 1000).getDay();
+          const day = days[weekDayIndex];
+
+          // temperatura minima e massima
+          const { min, max } = daily.temp;
+
+          // descizione previsione e indice dell'icona
+          const { description, icon } = daily.weather[0];
+
+          return {
+            key: timestamp,
+            day,
+            min,
+            max,
+            description,
+            icon,
+          };
+        });
+      })
       .then((forecasts) => dispatch(fetchForecastsSuccess(forecasts)))
       .catch((error) => dispatch(fetchForecastsError(error.message)));
   };
